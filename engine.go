4eb56d2df1d58a09b7cd5e54bb5a187cffa14467
saarow <saarow.dev@gmail.com>
2025-07-09 19:11:55 +0330

    feat(engine): add configuration options

--- engine.go
+++ engine.go
@@ -13,8 +13,14 @@ type Engine struct {
 	Proc   *Process
 	Ctx    context.Context
 	Cancel context.CancelFunc
+	Opts   EngineOptions
 
 	OutputChan chan string
+}
+
+type EngineOptions struct {
+	Depth   int
+	MultiPV int
 }
 
 func NewEngine(enginePath string) (*Engine, error) {
@@ -25,11 +31,17 @@ func NewEngine(enginePath string) (*Engine, error) {
 
 	ctx, cancel := context.WithCancel(context.Background())
 
+	opts := EngineOptions{
+		Depth:   20,
+		MultiPV: 1,
+	}
+
 	return &Engine{
 		Path:       enginePath,
 		Proc:       proc,
 		Ctx:        ctx,
 		Cancel:     cancel,
+		Opts:       opts,
 		OutputChan: make(chan string),
 	}, nil
 }
@@ -38,6 +50,10 @@ func (e *Engine) Run() error {
 	if err := e.Proc.Start(); err != nil {
 		return err
 	}
+
+	e.SendCommand("uci")
+	go e.ReadOutput()
+
 	return nil
 }
 
@@ -56,4 +72,38 @@ func (e *Engine) ReadOutput() {
 		// TODO: error handling
 		return
 	}
+}
+
+func (e *Engine) SetOption(name string, value any) error {
+	switch name {
+	case "depth":
+		depth, ok := value.(int)
+		if !ok {
+			return fmt.Errorf(
+				"option 'depth' requires integer value, got %T",
+				value,
+			)
+		}
+		e.Opts.Depth = depth
+
+	case "multipv":
+		mulitpv, ok := value.(int)
+		if !ok {
+			return fmt.Errorf(
+				"option 'multipv' requires integer value, got %T",
+				value,
+			)
+		} else if mulitpv > 256 || mulitpv < 1 {
+			return fmt.Errorf(
+				"multipv value must be between 1 and 256 (got %d)",
+				mulitpv,
+			)
+
+		}
+		e.Opts.MultiPV = mulitpv
+
+	default:
+		return fmt.Errorf("invalid option '%s'", name)
+	}
+	return nil
 }
